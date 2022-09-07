4a7e765fc7a8c5c0b5ab32373e03749e4b24b458
LEEYOSEPH <laksmi0628@gmail.com>
2022-09-08 08:25:46 +0900

    feat: Account Controller 생성 및 Test code 작성

---
+++ src/main/java/com/jpagesipan/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.jpagesipan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * Spring Security 5.7.x 부터 WebSecurityConfigurerAdapter 는 Deprecated.
+     * -> SecurityFilterChain, WebSecurityCustomizer 를 상황에 따라 빈으로 등록해 사용한다.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                        "/email-login", "/check-email-login", "login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+}
---
+++ src/test/java/com/jpagesipan/config/SecurityConfigTest.java
@@ -0,0 +1,10 @@
+package com.jpagesipan.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SecurityConfigTest {
+
+}
