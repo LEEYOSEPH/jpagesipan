7b0a847e9e4343a0b38925663994c14bcde2bd65
LEEYOSEPH <laksmi0628@gmail.com>
2022-09-07 22:00:34 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/jpagesipan/JpagesipanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpagesipan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpagesipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
