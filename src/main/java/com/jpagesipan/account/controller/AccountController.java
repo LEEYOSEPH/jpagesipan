c778185fa7632cb2052c498f3065d71c2f12402f
LEEYOSEPH <laksmi0628@gmail.com>
2022-09-13 21:17:41 +0900

    feat: 회원가입 폼 서브밋 처리

--- src/main/java/com/jpagesipan/account/controller/AccountController.java
+++ src/main/java/com/jpagesipan/account/controller/AccountController.java
@@ -1,7 +1,11 @@
 package com.jpagesipan.account.controller;
 
+import com.jpagesipan.account.domain.Account;
 import com.jpagesipan.account.dto.SignUpForm;
+import com.jpagesipan.account.reository.AccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +21,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     /*signUpForm 이라는 데인터를 받을 때 바인딩 설정*/
     @InitBinder("signUpForm")
@@ -38,6 +44,26 @@ public class AccountController {
         if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // encoding 필요
+                .studyCreateByWeb(true)
+                .studyEnrollmentResultByWeb(true)
+                .studyUpdatedByWeb(true)
+                .build();
+        Account newAccount = accountRepository.save(account);
+
+        //토큰값 생성
+        newAccount.generateEmailCheckToken();
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setText("게시판, 회원가입인증");
+        simpleMailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                "&email=" + newAccount.getEmail());
+
+        javaMailSender.send(simpleMailMessage);
 
         //회원가입 처리
         return "redirect:/";
